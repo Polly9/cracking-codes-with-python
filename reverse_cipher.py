e9e2deae8f795cd49862a3299306fc87dd976d08
Polly9 <polly9099@gmail.com>
2022-11-22 07:23:24 +0900

    first commit

---
+++ reverse_cipher.py
@@ -0,0 +1,14 @@
+def reverse_cipher(text):
+    """
+    入力した文字列を逆順で返す
+    """
+    translated = ''
+    span = len(text)
+    for i in range(span - 1, -1, -1):
+        translated += text[i]
+    return translated
+
+if __name__ == '__main__':
+    print('↓insert text↓')
+    text = input()
+    print(reverse_cipher(text))
